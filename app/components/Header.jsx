afdc16c029ad26c1f06e838a5089d741eb3894c6
kaled <kaledbit@gmail.com>
2025-07-17 13:40:28 +0300

    betar and thaer are not here part2

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -3,10 +3,13 @@ import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
+
 
 export default function Header() {
 
   const [isCookie,setIsCookie] = useState(false);
+  const [id,setId] = useState('');
   useEffect(() => {
     const checkCookie = () => {
       setIsCookie(!!Cookies.get('token'));
@@ -19,11 +22,18 @@ export default function Header() {
     const interval = setInterval(checkCookie, 1000);
     return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+      const token = Cookies.get('token');
+      const c = jwtDecode(token);
+      setId(c.userId);
+  },[])
   
   return (
     <header className="flex h-20 justify-between items-center p-4 bg-gray-100 mb-8">
       <Image src="/Logo.png" width={65} height={65} alt="Logo" className="rounded-full"/>
       {!isCookie && <Link href={'/login'} className="px-6 py-2 bg-gray-200 rounded-full cursor-pointer">Login</Link>}
+      {isCookie && <Link href={`/profile/${id}`} className="px-6 py-2 bg-gray-200 rounded-full cursor-pointer">profile</Link>}
   </header>
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,33 +3,13 @@ import { useState } from "react"
 import Filters from "./components/Filters"
 import PostsFetch from "./components/PostsFetch"
 import Sort from "./components/Sort"
+import Tabs from "./components/Tabs"
 
 function Home() {
   const [listType,setListType] = useState('apartments');
   return (
     <div>
-      <div className="w-4/5 mx-auto flex justify-between gap-4 my-5 flex-wrap">
-        <div className="flex-1 bg-cyan-500 p-4 rounded-lg cursor-pointer hover:opacity-90 transition-all text-white text-center font-medium"
-         onClick={() => setListType('apartments')}
-        >
-          Apartments
-        </div>
-        <div className="flex-1 bg-fuchsia-500 p-4 rounded-lg cursor-pointer hover:opacity-90 transition-all text-white text-center font-medium"
-         onClick={() => setListType('books')}
-        >
-          Books
-        </div>
-        <div className="flex-1 bg-yellow-500 p-4 rounded-lg cursor-pointer hover:opacity-90 transition-all text-white text-center font-medium"
-         onClick={() => setListType('cars')}
-        >
-          Cars
-        </div>
-        <div className="flex-1 bg-black p-4 rounded-lg cursor-pointer hover:opacity-90 transition-all text-white text-center font-medium"
-         onClick={() => setListType('laptops')}
-        >
-          Laptops
-        </div>
-      </div>
+     <Tabs setListType={setListType} />
 
       <div className="flex flex-col lg:flex-row gap-4">
         <div className="flex flex-col lg:w-1/5 gap-4 order-1 lg:order-1">
